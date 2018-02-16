e3c4d33f37c30edec8532531ca2877f53fd28436
DerutDan <danilderut@gmail.com>
2018-02-16 11:17:07 +0300

    Upd

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ var count = 0;
 
 $(function(){
   $("#root").append("<ul id = list> </ul>");
-  $("#list").append("<li id = " + count.toString() + "><span>Сделать задание #3 по web-программированию</span><button onClick = delete(" + count.toString() + ")>Удалить</button></li>");
+  $("#list").append("<li id = " + count.toString() + "><span>Сделать задание #3 по web-программированию</span><button onClick = dlt(" + count.toString() + ")>Удалить</button></li>");
   $("#root").append("<input id = add_task_input>");
   $("#root").append("<button onClick=addTask() id = add_task></button>");
  
   count = count + 1; })
 
-function delete(id){
+function dlt(id){
   $("#" + id).remove();
 }
 
 function addTask(){
-  $("#list").append("<li id = " + count.toString() + "><span>" + document.getElementById("add_task_input").value + "</span><button onClick = delete(" + count.toString() + ")>Удалить</button></li>");
+  $("#list").append("<li id = " + count.toString() + "><span>" + document.getElementById("add_task_input").value + "</span><button onClick = dlt(" + count.toString() + ")>Удалить</button></li>");
   count = count + 1;
 }
 
